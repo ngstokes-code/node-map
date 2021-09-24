cd32b00b327e4e4842bb7587e9ed6e858629bae2
Nicholas Stokes <ngstokes@buffalo.edu>
2021-09-24 19:17:53 -0400

    Navbar and load.html added. Load.html allows for uploading csv coordinate files, and saving/loading entire topologies. Basestation and marker placing functionality added

--- app.py
+++ app.py
@@ -1,45 +1,68 @@
-from flask import Flask, send_file, json, jsonify, render_template, request # flask imports
-import csv  # to convert database to a downloadable csv file
-import sqlite3    # database library
-# import matplotlib                      # to graph the database
-# from matplotlib.figure import Figure   # more efficient graphing
-# import matplotlib.pyplot as plt
+# flask imports
+from flask import Flask, flash, send_file, json, jsonify, render_template, request, redirect, Response, make_response
+import threading  # for mutex
+import csv       # to read / write csv files
+import sqlite3
+from flask.helpers import url_for   # database library
+
+from werkzeug.utils import secure_filename
+from werkzeug.wrappers import response
 
 app = Flask(__name__)
-app.url_map.strict_slashes = False
+# app.secret_key = 'asdjp12319c0asdklanc'
+
+app.config['UPLOAD_FOLDER'] = 'static/uploads'
+ALLOWED_EXTENSIONS = {'txt', 'csv'}
+
 
 @app.route('/')
 def home():
-   return render_template('index.html')
+    return render_template('index.html')
 
-@app.route('/receiver', methods = ['GET', 'POST'])
+
+@app.route('/load')
+def load():
+    return render_template('load.html')
+
+
+@app.route('/load_error')
+def load_error(error):
+    return render_template('load.html', error=error)
+
+
+@app.route('/receiver', methods=['GET', 'POST'])
 def get_node_data():
-    data = request.get_json(force = True)
-    lat, lng = data[0], data[1] # list[float] of lat / lng coordinates
-    # print(f'lat: {data[0]}, lng: {data[1]}')
-    connection = sqlite3.connect('coords.db')
-    cursor = connection.cursor()
-    cursor.execute("CREATE TABLE IF NOT EXISTS coordinates(lat REAL, lng REAL)")
-    
-    for lat, lng in zip(lat, lng):
-        cursor.execute("INSERT INTO coordinates VALUES ({}, {})".format(lat, lng))
+    if request.method == 'POST':
+        data = request.get_json(force=True)
+        drop_table()  # don't want to incrementally add nodes to database
+        lat, lng = data[0], data[1]  # list[float] of lat / lng coordinates
+        connection = sqlite3.connect('coords.db')
+        cursor = connection.cursor()
+        cursor.execute(
+            "CREATE TABLE IF NOT EXISTS coordinates(lat REAL, lng REAL)")
 
-    connection.commit()
+        for lat, lng in zip(lat, lng):
+            cursor.execute(
+                "INSERT INTO coordinates VALUES ({}, {})".format(lat, lng))
+
+        connection.commit()
 
     return "success"
+
 
 @app.route('/tabulate_database')
 def tabulate_database():
     connection = sqlite3.connect('coords.db')
     cursor = connection.cursor()
-    cursor.execute("CREATE TABLE IF NOT EXISTS coordinates(lat REAL, lng REAL)")
-    cursor.execute("SELECT * FROM coordinates") # asterisk (*) --> everything in the database
-    results = cursor.fetchall() # list of tuple(lat, lng)
-    
-    # strings are immutable in python, instead we use a list to avoid changing an immutable string
-    row_list = [] 
-    for idx, coordinate in enumerate(results): # enumerate so we can display node #
-        row =   """
+    cursor.execute(
+        "CREATE TABLE IF NOT EXISTS coordinates(lat REAL, lng REAL)")
+    # asterisk (*) --> everything in the database
+    cursor.execute("SELECT * FROM coordinates")
+    results = cursor.fetchall()  # list of tuple(lat, lng)
+
+    row_list = []  # strings are immutable in python, instead we use a list to avoid changing an immutable string
+    for idx, coordinate in enumerate(results):
+        row = """
             <tr>
                 <th scope="row">{num}</th>
                 <td>{lat}</td>
@@ -48,7 +71,7 @@ def tabulate_database():
                 """.format(num=(idx+1), lat=coordinate[0], lon=coordinate[1])
         row_list.append(row)
 
-    div_text =  """
+    div_text = """
         <table class="table table-striped table-dark">
             <thead>
               <tr>
@@ -62,8 +85,9 @@ def tabulate_database():
             </tbody>
         </table>
                 """.format(row_text='\n'.join(row_list))
-    
-    return jsonify(div_text) # jsonify the div_text and send it back to the html page
+
+    return jsonify(div_text)
+
 
 @app.route('/drop_db')
 def drop_table():
@@ -73,36 +97,45 @@ def drop_table():
     cursor.execute("DROP TABLE IF EXISTS coordinates")
     connection.commit()
     # wipe database and return index.html render template
-    return render_template('index.html')
+    # return render_template('index.html')
+    return "success"
+
 
 @app.route('/download_db')
 def download_db():
     return send_file('coords.db', as_attachment=True, download_name="node-positions.db")
+
 
 @app.route('/download_txt')
 def download_txt():
     # return send_file('coords.txt', as_attachment=True)
     connection = sqlite3.connect('coords.db')
     cursor = connection.cursor()
-    cursor.execute("CREATE TABLE IF NOT EXISTS coordinates(lat REAL, lng REAL)")
-    cursor.execute("SELECT * FROM coordinates") # asterisk (*) --> everything in the database
-    results = cursor.fetchall() # list of tuple(lat, lng)
-    
+    cursor.execute(
+        "CREATE TABLE IF NOT EXISTS coordinates(lat REAL, lng REAL)")
+    # asterisk (*) --> everything in the database
+    cursor.execute("SELECT * FROM coordinates")
+    results = cursor.fetchall()  # list of tuple(lat, lng)
+
     # Write the coordinates to a text file. I could've looped over and written line by line but this is faster I think
-    coords_to_string = '\n'.join(map(lambda i: str(i[0]) + ', ' + str(i[1]), results))
-    file = open("node-positions.txt", "w+") # open (create) a writable file
-    file.write(f"Latitude, Longitude\n{coords_to_string}") # write to the file
+    coords_to_string = '\n'.join(
+        map(lambda i: str(i[0]) + ', ' + str(i[1]), results))
+    file = open("node-positions.txt", "w+")  # open (create) a writable file
+    file.write(f"Latitude, Longitude\n{coords_to_string}")  # write to the file
     file.close()
 
     return send_file('node-positions.txt', as_attachment=True)
+
 
 @app.route('/download_csv')
 def download_csv():
     connection = sqlite3.connect('coords.db')
     cursor = connection.cursor()
-    cursor.execute("CREATE TABLE IF NOT EXISTS coordinates(lat REAL, lng REAL)")
-    cursor.execute("SELECT * FROM coordinates") # asterisk (*) --> everything in the database
-    results = cursor.fetchall() # list of tuple(lat, lng)
+    cursor.execute(
+        "CREATE TABLE IF NOT EXISTS coordinates(lat REAL, lng REAL)")
+    # asterisk (*) --> everything in the database
+    cursor.execute("SELECT * FROM coordinates")
+    results = cursor.fetchall()  # list of tuple(lat, lng)
 
     with open('node-positions.csv', 'w') as f:
         writer = csv.writer(f, lineterminator='\n')
@@ -114,5 +147,157 @@ def download_csv():
     return send_file('node-positions.csv', as_attachment=True)
 
 
+# mutex = threading.Lock()
+@app.route('/saveTopologyData', methods=['POST'])
+def req_save_topology_data():  # called when user wants to SAVE topology data
+    # mutex.acquire()
+    error = ""
+    data = request.get_json(force=True)
+    topology_id = data['id']    # Must check validity of 'topology_id'...
+    if not topology_id.isalnum():   # Invalid ID submitted by user, returning here before DB insert
+        error = "invalid ID (alphanumeric characters only)"
+    else:
+        error = save_topology(data)  # inserts data
+    # mutex.release()
+    # This is being sent back to JS, so render_template wont do anything. (No way around it)
+    return error
+
+
+# To explain the database structure for future developers;
+# The datatype BLOB is an unspecified type, but we're storing strings there (hence the single quotes).
+# This is because they are JSON STRINGS, so when we fetch the data we just decode the JSON and we have the actual datatype (list, etc)
+# Returns whether or not there was an error, inserts topology if no error
+def save_topology(data):
+    # Must parse the data, impossible to query the db as json (for future search feature)
+    topology_id = data['id']  # requested ID to save as
+    lat, lng = data['coordinates']  # list of [[lat], [lng]] -> unpack them
+    # total number of nodes (inside and outside polygon, ALL nodes)
+    num_nodes = data['numNodes']
+    basestations = data['stations']
+    num_stations = data['numStations']
+    # coordinates of polygon corners (empty if polygon unchanged)
+    polygon_coordinates = data['polyCoords']
+    polygon_area = data['polyArea']  # area (km^2) of polygon
+    # (nodes inside polygon) / (km^2 area of polygon)
+    polygon_ndensity = data['polyDensity']
+
+
+# "basestations": entry[4],
+# "numStations":entry[5],
+    # Save data to database if ID not taken
+    # table name == database name (not required, just simpler)
+    connection = sqlite3.connect('topologies.db')
+    cursor = connection.cursor()
+
+    create_table = """CREATE TABLE IF NOT EXISTS topologies(
+        id TEXT PRIMARY KEY, 
+        lat BLOB, lng BLOB, 
+        num_nodes INTEGER, 
+        basestations BLOB, 
+        num_stations INTEGER, 
+        polygon_coordinates BLOB, 
+        polygon_area REAL, 
+        polygon_ndensity REAL)"""
+    cursor.execute(create_table)
+
+    check_id = """SELECT id FROM topologies WHERE id='{}'""".format(
+        topology_id)
+    cursor.execute(check_id)
+    entry = cursor.fetchall()
+    error = ""
+    if bool(entry):  # list not empty --> database found existing ID
+        error = "ID already taken"
+    else:
+        print("Inserting into database")
+        insert_data = """INSERT INTO topologies VALUES ('{}', '{}', '{}', {}, '{}', {}, '{}', {}, {})""".format(topology_id, json.dumps(
+            lat), json.dumps(lng), num_nodes, json.dumps(basestations), num_stations, json.dumps(polygon_coordinates), polygon_area, polygon_ndensity)
+        cursor.execute(insert_data)
+        connection.commit()  # save changes
+
+    return error
+
+
+@app.route('/loadTopologyData', methods=['POST'])
+def req_load_topology_data():
+    data = request.get_json(force=True)
+    topology_id = data['id']    # Must check validity of 'topology_id'...
+    if not topology_id.isalnum():   # Invalid ID submitted by user, returning here before DB insert
+        # 400 BAD REQUEST
+        return make_response(("Invalid ID (alphanumeric characters only)", 400))
+    # tuple(error, data) containing data if there is no error
+    entry = load_topology(topology_id)
+    if entry == None:
+        return make_response(("ID does not exist", 404))  # 404 NOT FOUND
+    return json.dumps(entry)
+    # return make_response((json.dumps(entry), 200))
+    # return make_response((json.dumps("ID does not exist"), 404))
+
+
+def load_topology(id):  # return entry w/ given ID from database
+    # table name = database name (not required, just simpler)
+    connection = sqlite3.connect('topologies.db')
+    cursor = connection.cursor()
+
+    create_table = """CREATE TABLE IF NOT EXISTS topologies(
+        id TEXT PRIMARY KEY, 
+        lat BLOB, lng BLOB, 
+        num_nodes INTEGER,
+        basestations BLOB,
+        num_stations INTEGER, 
+        polygon_coordinates BLOB, 
+        polygon_area REAL, 
+        polygon_ndensity REAL)"""
+    cursor.execute(create_table)
+
+    check_id = """SELECT * FROM topologies WHERE id='{}'""".format(id)
+    cursor.execute(check_id)
+    entry = cursor.fetchall()
+    if bool(entry):  # entry found
+        entry = entry[0]
+        format_data = {
+            "id": entry[0],
+            "lat": entry[1],
+            "lng": entry[2],
+            "numNodes": entry[3],
+            "basestations": entry[4],
+            "numStations": entry[5],
+            "polyCoords": entry[6],
+            "polyArea": entry[7],
+            "polyDensity": entry[8]
+        }
+        return format_data
+    return None  # entry not present
+
+
+def print_topology_db():  # call this to print the topologies database
+    # table name = database name (not required, just simpler)
+    connection = sqlite3.connect('topologies.db')
+    cursor = connection.cursor()
+
+    create_table = """CREATE TABLE IF NOT EXISTS topologies(
+        id TEXT PRIMARY KEY, 
+        lat BLOB, lng BLOB, 
+        num_nodes INTEGER, 
+        basestations BLOB, 
+        num_stations INTEGER, 
+        polygon_coordinates BLOB, 
+        polygon_area REAL, 
+        polygon_ndensity REAL)"""
+    cursor.execute(create_table)
+
+    cursor.execute("SELECT * FROM topologies")
+    results = cursor.fetchall()
+    print(results)
+
+
 if __name__ == '__main__':
     app.run(debug=True)
+
+# Old DB print statements
+# print(f'ID: {topology_id},\nlat: {lat},\nlng: {lng},\nnum_nodes: {num_nodes},\npoly_coords: {polygon_coordinates},\npoly_area: {polygon_area},\npoly_ndensity: {polygon_ndensity}')
+# print(f"lat: {type(json.dumps(lat))}")
+# print(f"lng: {type(json.dumps(lng))}")
+# print(f"num_nodes: {type(num_nodes)}")
+# print(f"polyCoords: {type(json.dumps(polygon_coordinates))}")
+# print(f"polyArea: {type(polygon_area)}")
+# print(f"polyNdensity: {type(polygon_ndensity)}")
--- static/js/script.js
+++ static/js/script.js
@@ -3,8 +3,11 @@ var area = "<i>update polygon to show area</i>";
 var vertices = [];
 var polygon;
 var markers = [];
+var stations = [];
 var markerCnt = 0;
 var ndensity = 0;
+var stationCnt = 0;
+var place_mode = 0;
 document.getElementById("myLog").innerHTML = area; //"Update Polygon to show area";
 
 function createMap() {
@@ -32,16 +35,17 @@ function createMap() {
         map: map,
         paths: polygonCoordinates,
         clickable: false,
-        strokeColor: 'red',
-        fillColor: 'red',
+        strokeColor: 'yellow',
+        fillColor: 'yellow',
         fillOpacity: 0.4,
         draggable: true,
         editable: true
     });
 
     document.getElementById("markerCnt").innerHTML = "# of nodes inside polygon: " + markerCnt;
+    document.getElementById("stationCnt").innerHTML = "# of stations inside polygon: " + stationCnt;
     document.getElementById("density").innerHTML = "Node Density: " + ndensity + " nodes per km&sup2;";
-
+    //----------------set_at & inser_at are for whenever the polygon is updated/changed
     google.maps.event.addListener(polygon.getPath(), 'set_at', function () {
         PolyArray(polygon.getPath());
         area = google.maps.geometry.spherical.computeArea(polygon.getPath());
@@ -49,6 +53,7 @@ function createMap() {
         // console.log("Area inside highlighted area: " + kmarea + " kilometers squared");
         document.getElementById("myLog").innerHTML = "Polygon Area: " + kmarea.toFixed(6) + " km&sup2;";
         countMarkersInPolygon();
+        countStationsInPolygon();
         nodeDensity();
     });
     google.maps.event.addListener(polygon.getPath(), 'insert_at', function () {
@@ -58,6 +63,7 @@ function createMap() {
         console.log("Area inside highlighted area: " + kmarea + " kilometers squared");
         document.getElementById("myLog").innerHTML = "Polygon Area: " + kmarea.toFixed(6) + " km&sup2;";
         countMarkersInPolygon();
+        countStationsInPolygon();
         nodeDensity();
     });
 
@@ -69,6 +75,10 @@ function createMap() {
         clearMarkers();
         ndensity = 0;
     });
+    document.getElementById("clear-stations").addEventListener("click", function () {
+        clearStations();
+        //ndensity = 0;
+    });
     document.getElementById("polygon-move").addEventListener("click", function () {
         polygon.setOptions({ clickable: true });
     });
@@ -76,9 +86,14 @@ function createMap() {
         polygon.setOptions({ clickable: false });
 
     });
-    /*document.getElementById("random-markers").addEventListener("click", function(){
-        randomMarkers();
-    });*/
+    //Manually Place Markers or Stations
+    document.getElementById("click-markers").addEventListener("click", function () {
+        place_mode = 1;
+    });
+    document.getElementById("click-stations").addEventListener("click", function () {
+        place_mode = 2;
+
+    });
     document.getElementById("submit").addEventListener("click", function () {
         event.preventDefault()
         randomMarkers();
@@ -97,28 +112,61 @@ function createMap() {
         markerPositions();
     });
 }
-
+//-----------Adds markers and stations manually
 function addMarker(position) {
-    const marker = new google.maps.Marker({
-        position,
-        map,
-    });
-    markers.push(marker);
+    //For Stations???????
+    if( place_mode == 2){
+        const image = { url: "https://imagizer.imageshack.com/v2/512x512q90/r/924/ir66hA.png"/*"http://maps.google.com/mapfiles/kml/shapes/mechanic.png"*/,  scaledSize: new google.maps.Size(25, 25), origin: new google.maps.Point(0, 0), anchor: new google.maps.Point(0, 0)/*"http://maps.google.com/mapfiles/kml/shapes/placemark_circle.png"*/,};
+        const marker = new google.maps.Marker({
+            position,
+            map,
+            icon: image,
+        });
+        stations.push(marker);
+    }
+    //For Markers???????
+    if( place_mode == 1){
+    const image = { url: "http://maps.google.com/mapfiles/kml/shapes/placemark_circle.png", scaledSize: new google.maps.Size(20, 20),};
+        const marker = new google.maps.Marker({
+            position,
+            map,
+            icon: image,
+        });
+        markers.push(marker);
+    }
+    //console.log(stations);
     countMarkersInPolygon();
+    countStationsInPolygon();
 }
 
-function setMapOnAll(map) {
+function setMapOnAll_markers(map) {
     for (let i = 0; i < markers.length; i++) {
         markers[i].setMap(map);
     }
+
+}
+function setMapOnAll_stations(map) {
+    for (let i = 0; i < stations.length; i++) {
+        stations[i].setMap(map);
+    }
+
 }
 
+//--------------------Clear Markers and Stations
 function clearMarkers() {
-    setMapOnAll(null);
+    setMapOnAll_markers(null);
     markers = [];
+    //stations = [];
     document.getElementById("markerCnt").innerHTML = "# of nodes inside polygon: 0";
 }
 
+function clearStations() {
+    setMapOnAll_stations(null);
+    //markers = [];
+    stations = [];
+    document.getElementById("stationCnt").innerHTML = "# of stations inside polygon: 0";
+}
+//---------------------Marker Locations
 function PolyArray(array) {
     vertices = [];
 
@@ -130,7 +178,7 @@ function PolyArray(array) {
     }
     console.log(vertices);
 }
-
+//----------------------Counts Markers in Polygon
 function countMarkersInPolygon() {
     // count markers
     markerCnt = 0;
@@ -141,38 +189,55 @@ function countMarkersInPolygon() {
     }
     document.getElementById("markerCnt").innerHTML = "# of nodes inside polygon: " + markerCnt;
 }
-
-function randomMarkers() {
+//----------------------Counts Stations in Polygon
+function countStationsInPolygon() {
+    // count stations
+    stationCnt = 0;
+    for (var i = 0; i < stations.length; i++) {
+        if (google.maps.geometry.poly.containsLocation(stations[i].getPosition(), polygon)) {
+            stationCnt++;
+        }
+    }
+    document.getElementById("stationCnt").innerHTML = "# of stations inside polygon: " + stationCnt;
+}
+//----------------------Creates Random markers
+async function randomMarkers() {
     clearMarkers();
     countMarkersInPolygon();
+    const image = { url: "http://maps.google.com/mapfiles/kml/shapes/placemark_circle.png", scaledSize: new google.maps.Size(20, 20),};
     var bounds = new google.maps.LatLngBounds();
     for (var i = 0; i < polygon.getPath().getLength(); i++) {
         bounds.extend(polygon.getPath().getAt(i));
     }
     var sw = bounds.getSouthWest();
     var ne = bounds.getNorthEast();
-    for (var i = 0; i < 1000; i++) {
+    for (var i = 0; i < 5000; i++) {            //Marker Creation (INTEGER) Limit--------------------------------------
         var ptLat = Math.random() * (ne.lat() - sw.lat()) + sw.lat();
         var ptLng = Math.random() * (ne.lng() - sw.lng()) + sw.lng();
         var point = new google.maps.LatLng(ptLat, ptLng);
         if (google.maps.geometry.poly.containsLocation(point, polygon) && getRandomInput() > 0) {
-            var markerz = new google.maps.Marker({ position: point, map: map });
+            var markerz = new google.maps.Marker({ position: point, map: map, icon: image, });
             markers.push(markerz);
+            if (i && i % 25 == 0){
+                await new Promise(r => setTimeout(r, 250));
+            }
             if (markers.length == getRandomInput()) {
                 countMarkersInPolygon();
                 break;
             }
             countMarkersInPolygon();
+            nodeDensity();
         }
     }
 }
+randomMarkers();
 
 function nodeDensity() {
     var kmarea = parseFloat(area) * (.000001);
     ndensity = markerCnt / kmarea;
     document.getElementById("density").innerHTML = "Node Density: " + ndensity.toFixed(2) + " nodes per km&sup2;";
 }
-
+//----------------------Gets input number for random input
 function getRandomInput() {
     return document.getElementById("rnode").value;
 }
